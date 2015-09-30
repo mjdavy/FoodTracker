3b620cb3f48725eea9b0921c8559c2ec8054fff2
Martin Davy <mjdavy@hotmail.com>
2015-09-30 13:05:34 -0400

    fffd

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Davy, Martin on 9/29/15.
+//  Copyright © 2015 Mandi. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView
+{
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        <#code#>
+    }
+    
+}
